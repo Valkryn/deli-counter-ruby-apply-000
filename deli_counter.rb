9684335b6576f008b083a65ded49e74f1d22b8d3
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-03-01 17:52:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,19 +13,3 @@ def line(array)
 end
 
 line(katz_deli)
-
-katz_deli = []
-
-def line(array)
-  current_line = "The line is currently:"
-  if array.length >=1
-    array.each_with_index do |customer , position|
-      current_line << " #{position + 1}.#{customer}"
-    end
-    puts current_line
-  else
-    puts  "The line is currently empty."
-  end
-end
-
-line("Ada", "Grace", "Kent")
