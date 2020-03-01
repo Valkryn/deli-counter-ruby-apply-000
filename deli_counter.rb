98e3241ee76ee382d05fc9e73b2df330769bb6ac
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-03-01 17:50:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = ["Calvin"]
+
+def line(array)
+  current_line = "The line is currently:"
+  if array.length >=1
+    array.each_with_index do |customer , position|
+      current_line << " #{position + 1}.#{customer}"
+    end
+    puts current_line
+  else
+    puts  "The line is currently empty."
+  end
+end
+
+line(katz_deli)
