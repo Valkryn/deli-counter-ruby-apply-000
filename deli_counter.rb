95b05d6fdd0cab87edb30b48b6847e2564b5de02
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-03-01 19:47:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   current_line = "The line is currently:"
   if array.length >=1
     array.each_with_index do |customer , position|
-      current_line << " #{position + 1}.#{customer}"
+      current_line << " #{position + 1}. #{customer}"
     end
     puts current_line
   else
